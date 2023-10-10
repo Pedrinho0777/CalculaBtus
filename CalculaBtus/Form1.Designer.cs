8e1726199152d9b1197c296814f082fa9e4eae98
Pedro <pedrodev312@outlook.com>
2023-10-10 16:21:49 -0300

    Commit

--- CalculaBtus/Form1.Designer.cs
+++ CalculaBtus/Form1.Designer.cs
@@ -40,10 +40,15 @@
             btn_CalculaBtus = new Button();
             lblResultado = new Label();
             label4 = new Label();
+            btn_Limpar = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
             SuspendLayout();
             // 
             // cboQuantidadeDePessoas
             // 
+            cboQuantidadeDePessoas.DropDownStyle = ComboBoxStyle.DropDownList;
             cboQuantidadeDePessoas.FormattingEnabled = true;
             cboQuantidadeDePessoas.Items.AddRange(new object[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
             cboQuantidadeDePessoas.Location = new Point(261, 86);
@@ -61,16 +66,17 @@
             // chkSolDaTarde
             // 
             chkSolDaTarde.AutoSize = true;
-            chkSolDaTarde.Location = new Point(275, 140);
+            chkSolDaTarde.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            chkSolDaTarde.Location = new Point(275, 126);
             chkSolDaTarde.Name = "chkSolDaTarde";
-            chkSolDaTarde.Size = new Size(88, 19);
+            chkSolDaTarde.Size = new Size(92, 19);
             chkSolDaTarde.TabIndex = 2;
             chkSolDaTarde.Text = "Sol da tarde";
             chkSolDaTarde.UseVisualStyleBackColor = true;
             // 
             // txtAreaEmMetrosQuadrados
             // 
-            txtAreaEmMetrosQuadrados.Location = new Point(77, 215);
+            txtAreaEmMetrosQuadrados.Location = new Point(77, 237);
             txtAreaEmMetrosQuadrados.Name = "txtAreaEmMetrosQuadrados";
             txtAreaEmMetrosQuadrados.Size = new Size(100, 23);
             txtAreaEmMetrosQuadrados.TabIndex = 3;
@@ -85,38 +91,39 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(95, 62);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(95, 66);
             label1.Name = "label1";
-            label1.Size = new Size(68, 21);
+            label1.Size = new Size(49, 15);
             label1.TabIndex = 5;
             label1.Text = "Largura";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(77, 112);
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(83, 117);
             label2.Name = "label2";
-            label2.Size = new Size(116, 21);
+            label2.Size = new Size(84, 15);
             label2.TabIndex = 6;
             label2.Text = "Comprimento";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(96, 194);
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(92, 221);
             label3.Name = "label3";
-            label3.Size = new Size(45, 21);
+            label3.Size = new Size(33, 15);
             label3.TabIndex = 7;
             label3.Text = "Área";
             // 
             // btn_calcularArea
             // 
-            btn_calcularArea.Location = new Point(88, 165);
+            btn_calcularArea.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_calcularArea.Location = new Point(88, 183);
             btn_calcularArea.Name = "btn_calcularArea";
-            btn_calcularArea.Size = new Size(75, 23);
+            btn_calcularArea.Size = new Size(75, 26);
             btn_calcularArea.TabIndex = 8;
             btn_calcularArea.Text = "Calcular";
             btn_calcularArea.UseVisualStyleBackColor = true;
@@ -124,9 +131,10 @@
             // 
             // btn_CalculaBtus
             // 
-            btn_CalculaBtus.Location = new Point(275, 177);
+            btn_CalculaBtus.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_CalculaBtus.Location = new Point(278, 176);
             btn_CalculaBtus.Name = "btn_CalculaBtus";
-            btn_CalculaBtus.Size = new Size(75, 23);
+            btn_CalculaBtus.Size = new Size(75, 25);
             btn_CalculaBtus.TabIndex = 9;
             btn_CalculaBtus.Text = "Calcular";
             btn_CalculaBtus.UseVisualStyleBackColor = true;
@@ -136,11 +144,11 @@
             // 
             lblResultado.AutoSize = true;
             lblResultado.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            lblResultado.Location = new Point(63, 368);
+            lblResultado.Location = new Point(18, 292);
             lblResultado.Name = "lblResultado";
-            lblResultado.Size = new Size(62, 15);
+            lblResultado.Size = new Size(65, 15);
             lblResultado.TabIndex = 10;
-            lblResultado.Text = "Resultado";
+            lblResultado.Text = "Resultado:";
             // 
             // label4
             // 
@@ -152,11 +160,56 @@
             label4.TabIndex = 11;
             label4.Text = "Descubra a Área Primeiro";
             // 
+            // btn_Limpar
+            // 
+            btn_Limpar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_Limpar.Location = new Point(215, 341);
+            btn_Limpar.Name = "btn_Limpar";
+            btn_Limpar.Size = new Size(75, 34);
+            btn_Limpar.TabIndex = 12;
+            btn_Limpar.Text = "Limpar";
+            btn_Limpar.UseVisualStyleBackColor = true;
+            btn_Limpar.Click += btn_Limpar_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(275, 158);
+            label5.Name = "label5";
+            label5.Size = new Size(78, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Calcular Btus";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(85, 165);
+            label6.Name = "label6";
+            label6.Size = new Size(79, 15);
+            label6.TabIndex = 14;
+            label6.Text = "Calcular Área";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(275, 66);
+            label7.Name = "label7";
+            label7.Size = new Size(73, 15);
+            label7.TabIndex = 15;
+            label7.Text = "Qnt Pessoas";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(658, 425);
+            ClientSize = new Size(559, 387);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(btn_Limpar);
             Controls.Add(label4);
             Controls.Add(lblResultado);
             Controls.Add(btn_CalculaBtus);
@@ -170,7 +223,9 @@
             Controls.Add(txt_Largura);
             Controls.Add(cboQuantidadeDePessoas);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -189,5 +244,9 @@
         private Button btn_CalculaBtus;
         private Label lblResultado;
         private Label label4;
+        private Button btn_Limpar;
+        private Label label5;
+        private Label label6;
+        private Label label7;
     }
 }
--- CalculaBtus/Form1.cs
+++ CalculaBtus/Form1.cs
@@ -19,6 +19,7 @@ namespace CalculaBtus
                 area = largura * comprimento;
                 txtAreaEmMetrosQuadrados.Text = area.ToString();
             }
+            //Trata o erro de formato inválido
             catch (FormatException)
             {
                 MessageBox.Show("Digite uma valor válido", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -29,32 +30,58 @@ namespace CalculaBtus
         {
             try
             {
-
                 int quantidadePessoa = int.Parse(cboQuantidadeDePessoas.Text);
                 double AreaEmMetros;
                 double result;
 
+                //Se selecionar com sol da tarde, ele faz esse calculo
                 if (chkSolDaTarde.Checked)
                 {
                     AreaEmMetros = double.Parse(txtAreaEmMetrosQuadrados.Text);
                     result = AreaEmMetros * 800;
                 }
+                //Se não selecionar com sol da tarde, ele faz esse calculo
                 else
                 {
                     AreaEmMetros = double.Parse(txtAreaEmMetrosQuadrados.Text);
                     result = AreaEmMetros * 600;
                 }
 
+                //No final ele soma o resultado + qntPessoas * 800
                 result += quantidadePessoa * 800;
 
+                //Mostra o resultado na tela para o user
                 lblResultado.Text = $"Área de {AreaEmMetros} metros quadrados," +
                       $" com {quantidadePessoa} pessoas usando o ambiente, será necessário {result} BTUs.";
             }
+            //Trata o erro de formato inválido
             catch (FormatException)
             {
                 MessageBox.Show("Digite uma valor válido", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             }
+        }
+        public void LimpaCampo()
+        {
+            //Método para limpar os campos
+            txt_Largura.Text = "";
+            txt_Comprimento.Text = "";
+            txtAreaEmMetrosQuadrados.Text = "";
+            lblResultado.Text = "";
+            cboQuantidadeDePessoas.Text = "";
+            chkSolDaTarde.Checked = false;
+        }
+
+        private void btn_Limpar_Click(object sender, EventArgs e)
+        {
+            //Chama o método para limpar os campos 
+            LimpaCampo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Já começa o programa com uma 0 pessoa selecionada
+            cboQuantidadeDePessoas.SelectedIndex = 0;
         }
     }
 }
